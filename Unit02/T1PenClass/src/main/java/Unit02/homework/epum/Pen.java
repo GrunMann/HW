3cedc8c0cc3f87e58f6ee2fde9ca7973a254d1cd
GrunMann <bulat.atnabaev@gmail.com>
2017-02-26 10:33:50 +0200

    Unit02 1st task 1st commit

---
+++ Unit02/T1PenClass/src/main/java/Unit02/homework/epum/Pen.java
@@ -0,0 +1,56 @@
+package Unit02.homework.epum;
+
+public class Pen {
+
+    private String colour;
+    private double price;
+
+    public Pen(String colour, double price) {
+        this.colour = colour;
+        this.price = price;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getColour() {
+
+        return colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Pen{" +
+                "colour='" + colour + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Pen pen = (Pen) object;
+
+        if (Double.compare(pen.price, price) != 0) return false;
+        return colour.equals(pen.colour);
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        long temp;
+        result = 31 * result + colour.hashCode();
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
